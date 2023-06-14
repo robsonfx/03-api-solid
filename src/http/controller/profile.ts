75cdb3b3d34e941c8b9a40844dd861c79569539b
Robson Souza <robsonfx@gmail.com>
2023-06-14 14:47:01 -0300

    middleware for auth routes

--- src/http/controller/profile.ts
+++ src/http/controller/profile.ts
@@ -1,10 +1,18 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  // metodo valdia se foi passado um token
-  await request.jwtVerify()
+  // console.log(request.user.sub)
+  const getUseProfile = makeGetUserProfileUseCase()
 
-  console.log(request.user.sub)
+  const { user } = await getUseProfile.execute({
+    userId: request.user.sub,
+  })
 
-  return reply.status(200).send()
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  })
 }
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,11 +2,12 @@ import { FastifyInstance } from 'fastify'
 import { register } from './controller/register'
 import { authenticate } from './controller/authenticate'
 import { profile } from './controller/profile'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate) // authenticate
 
   /** Se autenticado */
-  app.get('/me', profile)
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
