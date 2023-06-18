b74b7867920d69e096fbe4ff85df5edb99027673
Robson Souza <robsonfx@gmail.com>
2023-06-18 15:39:29 -0300

    ajuste rota

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -14,10 +14,11 @@ export async function checkInsRoutes(app: FastifyInstance) {
   app.get('/check-ins/history', history)
   app.get('/check-ins/metrics', metrics)
 
-  app.post(
-    '/gyms/:gymId/check-ins',
+  app.post('/gyms/:gymId/check-ins', create)
+
+  app.patch(
+    '/check-ins/:checkInId/validate',
     { onRequest: [verifyUserRole('ADMIN')] },
-    create,
+    validate,
   )
-  app.patch('/check-ins/:checkInId/validate', validate)
 }
